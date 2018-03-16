0deeaa06ca1800b9ad11cb07c7cd4534aeec30b2
coreyauger <coreyauger@gmail.com>
2018-03-15 23:56:28 -0700

    debug plot

--- lstm.py
+++ lstm.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import os
 import glob
@@ -6,6 +5,8 @@ import matplotlib.pyplot as plt
 import math
 import csv
 import random
+import json
+import pprint as pp
 import quant as q
 from keras.models import Sequential
 from keras.layers import Dense
@@ -16,41 +17,86 @@ from functools import reduce
 # fix random seed for reproducibility
 np.random.seed(90210)
 
-def loadData(subset = -1):    
-    path =r'/home/suroot/Documents/train/reg/ema-65-crossover' # path to data
+def loadData(subset = -1, loadDebug = False):    
+    path =r'/home/suroot/Documents/train/reg/22222c82-59d1-4c56-a661-3e8afa594e9a' # path to data
     allFiles = glob.glob(os.path.join(path, "data_*.csv"))
     if(subset > 0):
         allFiles = allFiles[0:subset]
     data = []
+    debug = []
     for file in allFiles:
+        print(file)
+        fileNum = file.split('/')[-1].replace("data_","").replace(".csv","")
         with open(file, 'r') as f:
-            data.append([float(val) for sublist in list(csv.reader(f)) for val in sublist])        
-    return np.array(data)
+            data.append([float(val) for sublist in list(csv.reader(f)) for val in sublist]) 
+        if loadDebug:       
+            with open(file.replace("data_"+fileNum+".csv","debug_"+fileNum+".csv")) as f:
+                debug.append(json.load(f))       
+    return (np.array(data), debug)
     
 
-def priceChangeToPrice(data, initial = 100):
-    return list(reduce(lambda x,y:  x + [ x[-1]+(x[-1]*y) ], data, list([initial]) ) ) 
+ 
 
-subset = 45
+subset = 38
 
-data = loadData(subset)
+data, debug = loadData(subset, loadDebug=True)
 print(data.shape)
+
 
 # verify that a random data point looks correct.
 sample = random.randint(0,subset-1)
 print("sample " + str(sample))
-sample90Min = ((data[sample,:])[1:181])
-sample90Min = sample90Min[::2]  # only want price
-graph = priceChangeToPrice(sample90Min)
 
-ema15 = q.calculateEma(graph, 15, usePriceAsInitial = True) 
-ema65 = q.calculateEma(graph, 65, usePriceAsInitial = True) 
-#print(ema15)
-plt.plot(graph)
-plt.plot(ema15)
-plt.plot(ema65)
+q.debugPlot(data[sample,:], debug[sample])
+
+'''
+sample1Min = ((data[sample,:])[0:181])
+sample5Min = ((data[sample,:])[181:361])
+#print(sample90Min)
+print(sample1Min.shape)
+sample1Min = sample1Min[1::2]  # only want price
+sample5Min = sample5Min[::2]
+#print(sample90Min.shape)
+trigger = debug[sample]["Trigger"]["parent"][0]
+trainingExampleId = debug[sample]["TrainingExample"]["id"]
+symbol = debug[sample]["TrainingExample"]["symbol"]["sym"]
+
+triggerData = [val for sublist in trigger["event"]["data"] for val in sublist]
+priceData = list(filter(lambda x: x["$type"] == "m.q.PriceTs", triggerData ))
+ema15Data = list(filter(lambda x: x["$type"] == "m.q.EmaTs" and x["data"]["timePeriod"] == 15, triggerData ))
+ema65Data = list(filter(lambda x: x["$type"] == "m.q.EmaTs" and x["data"]["timePeriod"] == 65, triggerData ))
+
+# we need to rewind these values through time now.
+rewindPrice1 = q.rewindPriceChangeToPrice(sample1Min[::-1], initial=priceData[0]["data"]["close"])
+rewindPrice5 = q.rewindPriceChangeToPrice(sample5Min[::-1], initial=priceData[0]["data"]["close"])
+print("15: "+str(ema15Data[0]["data"]["ema"]))
+print("65: "+str(ema65Data[0]["data"]["ema"]))
+rewindEma15 = q.rewindEma(rewindPrice1, 15, startEma = ema15Data[0]["data"]["ema"]) 
+rewindEma65 = q.rewindEma(rewindPrice1, 65, startEma = ema65Data[0]["data"]["ema"]) 
+print("rewindPrice1: " + str(rewindPrice1[-1]))
+print(rewindEma65)
+print("rewindEma15: " + str(rewindEma15[-1]))
+print("rewindEma65: " + str(rewindEma65[-1]))
+
+enterPrice = priceData[0]["data"]["close"]
+print("symbol: "+symbol)
+print("Training Example: " + trainingExampleId)
+print("enter price: " + str(enterPrice))
+print("enter time: " + priceData[0]["time"])
+graph1 = q.priceChangeToPrice(sample1Min, initial=rewindPrice1[-1])
+graph5 = q.priceChangeToPrice(sample5Min, initial=rewindPrice5[-1])
+#print(graph)
+#print(graph[-1])
+ema15 = q.calculateEma(graph1, 15, startEma=graph1[0]) 
+ema65 = q.calculateEma(graph1, 65, startEma=graph1[0]) 
+plt.plot(([graph1[0]] * 72*5) + graph1)
+plt.plot(([graph1[0]] * 72*5) + ema65)
+plt.plot(([graph1[0]] * 72*5) + ema15)
+graph5 = [[x]*5 for x in graph5]
+graph5 = [val for sublist in graph5 for val in sublist]
+plt.plot(graph5)
 plt.show()
-
+'''
 
 # create and fit the LSTM network
 '''
--- quant.py
+++ quant.py
@@ -1,15 +1,109 @@
 
 from functools import reduce
+import matplotlib.pyplot as plt
 
 
-def calculateEma(price, interval = 9, usePriceAsInitial = False):
+def calculateEma(price, interval = 9, startEma = -1):
     #https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp
+    # (Closing price-EMA(previous day)) x multiplier + EMA(previous day)
     k = 2/(interval + 1)
-    if usePriceAsInitial:
-        return reduce(lambda x,y: x + [ (y-x[-1]+1)*k + x[-1] ], price, [price[0]])
+    if startEma > 0:
+        return reduce(lambda x,y: x + [ (y - x[-1]) * k + x[-1] ], price, [startEma])
     else:        
         subset = price[0:interval]
         sma = sum(subset) / len(subset)
         start = [sma] * interval    
-        return reduce(lambda x,y: x + [ (y-x[-1]+1)*k + x[-1] ], price[interval:], start)    
+        return reduce(lambda x,y: x + [ (y - x[-1]) * k + x[-1] ], price[interval:], start) 
+
+def rewindEma(price, interval, startEma):
+    k = 2/(interval + 1)   
+    return reduce(lambda x,c: x + [ (-c*k + x[-1])  / (-k+1) ], price, [startEma])
+
+def priceChangeToPrice(data, initial = 100):    
+    return list(reduce(lambda x,y:  x + [ x[-1]+(x[-1]*y) ], data, list([initial]) ) )
+
+def rewindPriceChangeToPrice(data, initial = 100):
+    return list(reduce(lambda x,y:  x + [ x[-1] / (y+1.0) ], data, list([initial]) ) )
+
+
+def debugPlot(data, debug, timeDomains = [1,5]):
+    sample1Min = data[0:181]
+    
+    #print(sample90Min)
+    print(sample1Min.shape)
+    sample1Min = sample1Min[1::2]  # only want price
+    
+    #print(sample90Min.shape)
+    trigger = debug["Trigger"]["parent"][0]
+    trainingExampleId = debug["TrainingExample"]["id"]
+    symbol = debug["TrainingExample"]["symbol"]["sym"]
+
+    triggerData = [val for sublist in trigger["event"]["data"] for val in sublist]
+    priceData = list(filter(lambda x: x["$type"] == "m.q.PriceTs", triggerData ))
+    ema15Data = list(filter(lambda x: x["$type"] == "m.q.EmaTs" and x["data"]["timePeriod"] == 15, triggerData ))
+    ema65Data = list(filter(lambda x: x["$type"] == "m.q.EmaTs" and x["data"]["timePeriod"] == 65, triggerData ))
+
+    # we need to rewind these values through time now.
+    rewindPrice1 = rewindPriceChangeToPrice(sample1Min[::-1], initial=priceData[0]["data"]["close"])
+    
+    print("15: "+str(ema15Data[0]["data"]["ema"]))
+    print("65: "+str(ema65Data[0]["data"]["ema"]))
+    rewindEma15 = rewindEma(rewindPrice1, 15, startEma = ema15Data[0]["data"]["ema"]) 
+    rewindEma65 = rewindEma(rewindPrice1, 65, startEma = ema65Data[0]["data"]["ema"]) 
+    print("rewindPrice1: " + str(rewindPrice1[-1]))
+    print(rewindEma65)
+    print("rewindEma15: " + str(rewindEma15[-1]))
+    print("rewindEma65: " + str(rewindEma65[-1]))
+
+    enterPrice = priceData[0]["data"]["close"]
+    print("symbol: "+symbol)
+    print("Training Example: " + trainingExampleId)
+    print("enter price: " + str(enterPrice))
+    print("enter time: " + priceData[0]["time"])
+    graph1 = priceChangeToPrice(sample1Min, initial=rewindPrice1[-1])
+    
+    #print(graph)
+    #print(graph[-1])
+    ema15 = calculateEma(graph1, 15, startEma=graph1[0]) 
+    ema65 = calculateEma(graph1, 65, startEma=graph1[0]) 
+    series = [graph1, ema65, ema15]
+    ind = 1
+    
+    for t in filter(lambda x: x != 1,timeDomains):
+        start = (ind*180)+1
+        end = ((ind+1)*180)+1
+        sampleXMin = data[start:end]
+        sampleXMin = sampleXMin[::2]
+        rewindPriceX = rewindPriceChangeToPrice(sampleXMin[::-1], initial=priceData[0]["data"]["close"])
+        extra = 90*t - 90*timeDomains[ind-1]-timeDomains[ind-1] 
+        print("extra: "+ str(extra))
+        series = [([graph1[0]] * extra) + x for x in series]
+
+        graphX = priceChangeToPrice(sampleXMin, initial=rewindPriceX[-1])
+        graphX = [[x]*t for x in graphX]
+        graphX = [val for sublist in graphX for val in sublist]
+        print(len(graphX))
+        print(len(series[0]))
+        series.append(graphX)
+        ind = ind+1
+
+
+    for x in series:
+        plt.plot(x) 
+    plt.show()  
+    '''
+    sample5Min = data[181:361]
+    sample5Min = sample5Min[::2]
+    rewindPrice5 = rewindPriceChangeToPrice(sample5Min[::-1], initial=priceData[0]["data"]["close"])
+    graph5 = priceChangeToPrice(sample5Min, initial=rewindPrice5[-1])
+    plt.plot(([graph1[0]] * 72*5) + graph1)
+    plt.plot(([graph1[0]] * 72*5) + ema65)
+    plt.plot(([graph1[0]] * 72*5) + ema15)
+    graph5 = [[x]*5 for x in graph5]
+    graph5 = [val for sublist in graph5 for val in sublist]
+    plt.plot(graph5)
+    plt.show()
+    '''
+
+   
     
