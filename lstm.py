c3d05e800032bd4f6cc985e03823cdd1446cc9b6
coreyauger <coreyauger@gmail.com>
2018-03-18 17:38:50 -0700

    first run

--- lstm.py
+++ lstm.py
@@ -8,6 +8,7 @@ import random
 import json
 import pprint as pp
 import quant as q
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Bidirectional
@@ -33,49 +34,51 @@ def loadData(path, subset = -1, loadDebug = False):
     return (np.array(data), debug)
     
  
-subset = 32
+subset = 10000
 
 path =r'/home/suroot/Documents/train/reg/22222c82-59d1-4c56-a661-3e8afa594e9a' # path to data
-data, debug = loadData(path, subset, loadDebug=True)
+data, debug = loadData(path, subset, loadDebug=False)
 print(data.shape)
 
 # verify that a random data point looks correct.
-sample = random.randint(0,subset-1)
-print("sample " + str(sample))
-
-q.debugPlot(data[sample,:], debug[sample])
+#sample = random.randint(0,subset-1)
+#print("sample " + str(sample))
+#q.debugPlot(data[sample,:], debug[sample])
 
 batch_size=128
-nb_epoch = 500
+nb_epoch = 5
 
 scaler = MinMaxScaler(feature_range=(-1, 1))
 data = scaler.fit_transform(data)
 
-Y_train = data[:,0]
-Y_train = np.reshape(Y_train, (Y_train.shape[0],1))
-print("Y shape: "+str(Y_train.shape))
+y = data[:,0]
+y = np.reshape(y, (y.shape[0],1))
+X = data[:,1:]
+X = np.reshape(X, (X.shape[0],450,2))
 
-X_train = data[:,1:]
-X_train = np.reshape(X_train, (X_train.shape[0],450,2))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+print("Y shape: "+str(y_train.shape))
 print("X shape: "+str(X_train.shape))
+
 
 # create and fit the LSTM network
 model = Sequential()
 model.add(Bidirectional(LSTM(90, dropout=0.2, activation="tanh"), input_shape=(450, 2)) )
 #model.add(LSTM(90, input_shape=(450, 2), dropout=0.2, activation="tanh") )
 model.add(Dense(1, activation="linear"))
-model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['mean_absolute_percentage_error'])
 
 model.summary()
 
 # Train
-history = model.fit(X_train, Y_train, epochs=nb_epoch, batch_size=batch_size, shuffle=True, verbose=1)
+history = model.fit(X_train, y_train, epochs=nb_epoch, batch_size=batch_size, shuffle=True, verbose=1)
 
 # Evaluate train
-evaluation = model.evaluate(X_train, Y_train, batch_size=batch_size, verbose=2)
+evaluation = model.evaluate(X_train, y_train, batch_size=batch_size, verbose=2)
 print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
 
 # Evaluate test
-#evaluation = model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=1)
-#print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
+evaluation = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=2)
+print('Summary: Loss over the test dataset: %.2f, Accuracy: %.2f' % (evaluation[0], evaluation[1]))
 
